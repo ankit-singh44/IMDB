892ad425c4cc5a60b62f561e4b5e25d13076edd2
Ankit Singh <ankitsingh@192.168.1.2>
2025-02-06 19:15:51 +0530

    adding watchList data on Table

--- src/Components/Demo/Demo.jsx
+++ src/Components/Demo/Demo.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+return <div>
 
-const Demo = () => {
-  return (
-    <div className='h-[70px] bg-gray-400 w-full flex'>
-        <h1>Ankit</h1>
-    </div>
-  )
-}
+      {/* List of movies */}
+      <div className='flex justify-center m-6'>
+        {
+          genres.map((genre)=> {
+            return <div key={genre} className='flex items-center justify-center mx-4 bg-blue-400 h-10 w-28 text-white font-bold rounded-xl'>
+              {genre}
+            </div>
+          })
+        }
+      </div>
 
-export default Demo
+      {/* Search Bar for search fav movie */}
+      <div className='flex justify-center my-8'>
+        <input type="text" placeholder='Search Movies'
+        className='h-10 w-60 bg-gray-200 outline-none px-4'/>
+      </div>
+
+      {/* WatchList fav movie data presented in tabel */}
+      <div className='m-6 rounded-lg overflow-hidden border shadow-md'>
+        <table className='p-4 w-full text-center'>
+          <thead className='bg-gray-50 h-12 border-b-2'>
+            <tr>
+              <th>Name</th>
+              <th>Rating</th>
+              <th>Popularity</th>
+              <th>Genre</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              movies.map((movie)=>{
+                return <tr className="border-b-2">
+                  <td className='flex items-center mx-4 py-2'>
+                    <img className='h-52' src={`https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="Movie Image`} />
+                    {movies.title}
+                    <div className='font-medium mx-2 '>{movie.title}</div>
+                  </td>
+                  <td>{movie.vote_average}</td>
+                  <td>{movie.popularity}</td>
+                  <td>{genreids[movie.genre_ids[0]]}</td>
+                  <td className='text-red-500'>Delete</td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+  </div>
--- src/Components/WatchList/WatchList.jsx
+++ src/Components/WatchList/WatchList.jsx
@@ -11,10 +11,8 @@ const WatchList = () => {
     // from movie we retrieve the data and get the genre id and store them into set so that we can get unique Genre
     movies.forEach((movie)=>{
       const genreIds=movie.genre_ids;
-      const genreNames= genreIds.map((genreId)=> genreids[genreId]);
-      genreNames.forEach((genreName)=>{
+      const genreName= genreids[genreIds[0]];
         genreSet.add(genreName);
-      })
     })
 
     console.log(genreSet);
@@ -41,7 +39,8 @@ const WatchList = () => {
     }
     return <div>
 
-      <div className='flex justify-center m-2'>
+      {/* List of movies */}
+      <div className='flex justify-center m-6'>
         {
           genres.map((genre)=> {
             return <div key={genre} className='flex items-center justify-center mx-4 bg-blue-400 h-10 w-28 text-white font-bold rounded-xl'>
@@ -51,10 +50,45 @@ const WatchList = () => {
         }
       </div>
 
-      <div>
-        <input type="text" placeholder='Search Movies'/>
+      {/* Search Bar for search fav movie */}
+      <div className='flex justify-center my-8'>
+        <input type="text" placeholder='Search Movies'
+        className='h-10 w-60 bg-gray-200 outline-none px-4'/>
       </div>
 
+      {/* WatchList fav movie data presented in tabel */}
+      <div className='m-6 rounded-lg overflow-hidden border shadow-md'>
+        <table className='p-4 w-full text-center'>
+          <thead className='bg-gray-50 h-12 border-b-2'>
+            <tr>
+              <th>Name</th>
+              <th>Rating</th>
+              <th>Popularity</th>
+              <th>Genre</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              movies.map((movie)=>{
+                return <tr className="border">
+                  <td className='flex items-center mx-4 p-4'>
+                    <div className='flex items-center gap-4'>
+                      <img className='h-52' src={`https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="Movie Image`} />
+                      {movies.title}
+                      <span className='font-medium mx-2 '>{movie.title}</span>
+                    </div>
+                  </td>
+                  <td>{movie.vote_average}</td>
+                  <td>{movie.popularity}</td>
+                  <td>{genreids[movie.genre_ids[0]]}</td>
+                  <td className='text-red-500'>Delete</td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
 }
 
